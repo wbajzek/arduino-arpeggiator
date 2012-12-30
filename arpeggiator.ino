b41fab8d405c85f553e30c0f5ff2e72e21f7f29d
William Bajzek <williambajzek@gmail.com>
2012-12-29 23:27:36 -0600

    a bit of code cleanup and commenting

--- arpeggiator.ino
+++ arpeggiator.ino
@@ -72,8 +72,7 @@ void setup() {
   // Initiate MIDI communications, listen to all channels
   MIDI.begin(MIDI_CHANNEL_OMNI);    
 
-  // Connect the HandleNoteOn function to the library, so it is called upon reception of a NoteOn.
-  MIDI.setHandleNoteOn(HandleNoteOn);  // Put only the name of the function
+  MIDI.setHandleNoteOn(HandleNoteOn); 
   MIDI.setHandleControlChange (HandleControlChange);
   MIDI.setHandleClock (HandleClock);
   MIDI.setHandleStart (HandleStart);
@@ -87,68 +86,74 @@ void setup() {
 
 
 // This function will be automatically called when a NoteOn is received.
-// It must be a void-returning function with the correct parameters,
 // see documentation here: 
 // http://arduinomidilib.sourceforge.net/
 
 void HandleNoteOn(byte channel, byte pitch, byte velocity) { 
 
-  if (velocity == 0)
+
+  if (velocity == 0) // note released
     notesHeld--;
   else {
     // If it's in hold mode and you are not holding any notes down,
     // it continues to play the previous arpeggio. Once you press
     // a new note, it resets the arpeggio and starts a new one.
     if (notesHeld==0 && hold) 
-      resetNotes();      
+      resetNotes();
+
     notesHeld++;
   }
-  
-  
-  // Turn on an LED when notes are held.
+
+
+  // Turn on an LED when any notes are held and off when all are released.
   if (notesHeld > 0)
-    digitalWrite(STAT2,LOW);
+    digitalWrite(STAT2,LOW); // stupid midi shield has high/low backwards for the LEDs
   else
-    digitalWrite(STAT2,HIGH);
+    digitalWrite(STAT2,HIGH); // stupid midi shield has high/low backwards for the LEDs
 
 
 
-
+  // find the right place to insert the note in the notes array
   for (int i = 0; i < sizeof(notes); i++) {
-    
-    if (velocity == 0) {
-      if (!hold && notes[i] >= pitch) {
-        // remove this note
+
+    if (velocity == 0) { // note released
+      if (!hold && notes[i] >= pitch) { 
+
+        // shift all notes in the array beyond or equal to the
+        // note in question, thereby removing it and keeping
+        // the array compact.
         if (i < sizeof(notes))
           notes[i] = notes[i+1];
       }
     }
     else {
-      
+
       if (notes[i] == pitch)
         return;   // already in arpeggio
       else if (notes[i] != '\0' && notes[i] < pitch)
-        continue; // skip
+        continue; // ignore the notes below it
       else {
-        
-        // scoot the rest of the arpeggio array over
+        // once we reach the first note in the arpeggio that's higher
+        // than the new one, scoot the rest of the arpeggio array over 
+        // to the right
         for (int j = sizeof(notes); j > i; j--)
           notes[j] = notes[j-1];
-  
-        // insert the note
+
+        // and insert the note
         notes[i] = pitch;
-        return;
-        
+        return;        
       }
     }
   }
 }
 
-
+// just pass midi CC through
 void HandleControlChange (byte channel, byte number, byte value) {
   MIDI.sendControlChange(number,value,channel); 
 }
 
+// This is a midi clock sync "start" message. In this case, switch to clock
+// sync mode and trigger the first note.
 void HandleStart () {
   clockSync = true;  
   clockTick = 1;
@@ -157,30 +162,39 @@ void HandleStart () {
   tick = millis();
   sei();
 
-
   handleTick(tick);
 }
+
+// Turn clock sync off when "stop" is received.
 void HandleStop () {
   clockSync = false;
 }
+
+// on each tick of the midi clock, determine whether or note to play
+// a note
 void HandleClock () {
 
   cli();
   tick = millis();
   sei();
-  
+
+  // keep a counter of every clock tick we receive. if the count
+  // corresponds with the subdivision selected with the tempo knob,
+  // play the note and reset the clock.
+  //
+  // clock is 1 based instead of 0 for ease of calculation 
+
   if (clockTick >= (int)map(analogRead(1),0,1023,1,4)*6 + 1) {
     handleTick(tick);
     clockTick = 1;
   }
-  
-  
+
+
   clockTick++;
 }
 
 void loop() {
 
-  // Call MIDI.read the fastest you can for real-time performance.
   MIDI.read();
 
 
@@ -192,22 +206,28 @@ void loop() {
   handleButtonTwo();
   handleButtonThree();
 
+  // if not in  clock synch mode, we just read the tempo from the
+  // tempo knob. 
   if (!clockSync) {  
-    tempo = 6000 / ((127-analogRead(1)/8) + 10);
+    // There's no need to be precise about it here. This simple 
+    // calculation is done quickly and gives a very wide range.
+    tempo = 6000 / ((127-analogRead(1)/8) + 2);
     handleTick(tick);
   }
-  
+
 }
 
 void handleTick(unsigned long tick) {
-  
-  if (blinkOn && tick - blinkTime > 50) {
+
+  // leave the LED long enough to be brightish but not so long
+  // that it ends up being solid instead of blinking
+  if (blinkOn && tick - blinkTime > 10) {
     blinkOn=false;
-    digitalWrite(STAT1,HIGH);
+    digitalWrite(STAT1,HIGH); // stupid midi shield has high/low backwards for the LEDs
   }
   if (clockSync || tick - lastTime > tempo) {
     blinkTime = lastTime = tick;
-    digitalWrite(STAT1,LOW);
+    digitalWrite(STAT1,LOW); // stupid midi shield has high/low backwards for the LEDs
     blinkOn = true;
 
 
@@ -220,11 +240,11 @@ void handleTick(unsigned long tick) {
       // fixes a bug where a random note would sometimes get played when switching chords
       if (notes[playBeat] == '\0')
         playBeat = 0;
-       
+
       // play the current note
       MIDI.sendNoteOn(notes[playBeat],velocity(),CHANNEL);
 
-
+      // decide what the next note is based on the mode.
       if (mode == UP) 
         up();
       else if (mode == DOWN) 
@@ -237,7 +257,7 @@ void handleTick(unsigned long tick) {
         oneThree();
       else if (mode == ONETHREEEVEN)
         oneThreeEven();
-      
+
     }
   }
 }
@@ -330,14 +350,14 @@ void oneThree() {
 }
 
 void oneThreeEven() {
-  
+
   if (notes[playBeat+1] == '\0') {
     playBeat = 0;
     arpUp = true;
     return;
   }
-  
-  
+
+
   if (arpUp)
     playBeat += 2;
   else
@@ -345,22 +365,25 @@ void oneThreeEven() {
 
   arpUp = !arpUp;
 
-    
+
 }
 
-
+// empties out the arpeggio. used when switching modes, when in hold mode and
+// a new arpeggio is started, or when the reset button is pushed.
 void resetNotes() {
   for (int i = 0; i < sizeof(notes); i++)
     notes[i] = '\0';
 }
 
+
+// Basic code to read the buttons.
 char button(char button_num)
 {
   return (!(digitalRead(button_num)));
 }
 
 
-
+// Button one is the the Hold button.
 void handleButtonOne() {
   boolean buttonOnePressed = button(BUTTON1);
   if (buttonOnePressed) {
@@ -379,6 +402,7 @@ void handleButtonOne() {
   }
 }
 
+// Button two is the mode button.
 void handleButtonTwo() {
   boolean buttonTwoPressed = button(BUTTON2);
   if (buttonTwoPressed) {
@@ -402,6 +426,7 @@ void handleButtonTwo() {
   }
 }
 
+// button three is the reset button
 void handleButtonThree() {
   boolean buttonThreePressed = button(BUTTON3);
   if (buttonThreePressed) {
@@ -419,6 +444,8 @@ void handleButtonThree() {
     buttonThreeHeldTime = 0;
   }
 }
+
+
 
 
 
